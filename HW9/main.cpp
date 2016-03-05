b5f6b8fbadc93864d5a6d5a7d912d5ed01df8cde
Ruidong Dai <ruidongd@uci.edu>
2016-03-05 14:34:07 -0800

    Initial Commit

---
+++ HW9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW9
+//
+//  Created by DaiRuidong on 16/3/5.
+//  Copyright © 2016年 DaiRuidong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
